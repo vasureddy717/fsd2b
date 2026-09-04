9900a9772e507fd57117e3bd2a212d74474708e7
vasureddy717 <vasur717@gmail.com>
2026-09-04 21:18:43 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import CounterButtonFunc from './CounterButtonFunc';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <CounterButtonFunc />
-);
